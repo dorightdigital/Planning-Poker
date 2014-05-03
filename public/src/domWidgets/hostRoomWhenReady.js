ae09fa6bb754716007545fd94c3af3bcdbd3326b
Mat Carey <mat@matcarey.co.uk>
2014-05-03 02:51:42 +0100

    Linking to new room for easier testing.

--- public/src/domWidgets/hostRoomWhenReady.js
+++ public/src/domWidgets/hostRoomWhenReady.js
@@ -2,15 +2,24 @@ define(['jquery', 'apiClient'], function ($, api) {
   return {
     init: function ($elem) {
       api.onRoomReady(function (room) {
-        var fullRoomUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : (':' + window.location.port)) + room.url;
+        var portString = window.location.port === '' ? '' : (':' + window.location.port);
+        var fullRoomUrl = window.location.protocol + '//' + window.location.hostname + portString + room.url;
         $elem.html($('<h1/>').text('You\'re hosting room ' + room.name));
-        $elem.append($('<div/>').text('Request a vote ').append($('<form/>').submit(function (e) {
-          e.preventDefault();
-          api.requestVotes($(this).find('input').val());
-          return false;
-        }).append('<input/>')));
+        $elem.append($('<div/>').text('Request a vote ').append(
+          $('<form/>').submit(function (e) {
+            e.preventDefault();
+            api.requestVotes($(this).find('input').val());
+            return false;
+          }).append('<input/>')));
         $elem.append($('<ul data-dom-widget="incomingParticipantRequests"/>'));
-        $elem.append($('<div><h3>Others can join by visiting ' + fullRoomUrl + '</h3><span data-dom-widget="showQrCode" data-url="' + fullRoomUrl + '"/> </div>'));
+        $elem.append(
+          $('<div/>')
+            .append(
+              $('<h3>Others can join by visiting </h3>')
+                .append($('<a/>')
+                  .attr('href', fullRoomUrl)
+                  .text(fullRoomUrl)))
+            .append($('<span data-dom-widget="showQrCode"/>').attr('data-url', fullRoomUrl)));
         $elem.append($('<div data-dom-widget="listParticipants"/>'));
         $elem.loadDomWidgets();
       });
