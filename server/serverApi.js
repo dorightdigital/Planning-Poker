a5eb85101e200fb40c63f3e2d79cbdcd89fb8629
Mat Carey <mat@matcarey.co.uk>
2014-05-06 08:43:22 +0100

    Refactor

--- server/serverApi.js
+++ server/serverApi.js
@@ -6,61 +6,31 @@ exports.init = function (server) {
   io.sockets.on('connection', function (socket) {
     var user = require('./userManager').getFromSocket(socket);
 
+
+    function withSpecifiedRoom(ref, callback) {
+      var room = rooms.get(ref);
+      if (room) {
+        callback(room);
+      } else {
+        user.sendError('Room not found "' + ref + '"');
+      }
+    }
     function callWithRoomAndUser(config, fn) {
-      var room = rooms.get(config.roomRef);
-      if (!room) {
-        user.sendError('Room not found "' + config.roomRef + '"');
-        return;
-      }
-      var guest = users.getFromRef(config.userRef);
-      if (!guest) {
-        user.sendError('User not found "' + config.userRef + '"');
-        return;
-      }
-      room.actions[fn](guest, user);
+      withSpecifiedRoom(config.roomRef, function (room) {
+        var guest = users.getFromRef(config.userRef);
+        if (!guest) {
+          user.sendError('User not found "' + config.userRef + '"');
+          return;
+        }
+        room.actions[fn](guest, user);
+      });
     }
 
-//    var roomsUsedByCurrentSocket = {host: [], participant: []};
-//    socket.on('disconnect', function () {
-//      var participant = roomsUsedByCurrentSocket.participant;
-//      var host = roomsUsedByCurrentSocket.host;
-//      for (var i in  participant) {
-//        if (participant.hasOwnProperty(i)) {
-//          var room = participant[i];
-//          var id = room.participants.approved.indexOf(socket) || room.participants.pending.indexOf(socket);
-//          room.participants.host.socket.emit('participant-leave', {
-//            ref: id
-//          });
-//        }
-//      }
-//      for (var i in  host) {
-//        if (host.hasOwnProperty(i)) {
-//          var room = host[i];
-//          for (var j in room.participants.pending) {
-//            if (room.participants.pending.hasOwnProperty(j)) {
-//              room.participants.pending[j].socket.emit('room-close', {
-//                ref: room.info.ref
-//              });
-//            }
-//          }
-//          for (var j in room.participants.approved) {
-//            if (room.participants.approved.hasOwnProperty(j)) {
-//              room.participants.approved[j].socket.emit('room-close', {
-//                ref: room.info.ref
-//              });
-//            }
-//          }
-//        }
-//      }
-//    });
     socket.on('join-room', function (config) {
-      var room = rooms.get(config.ref);
       user.setName(config.name);
-      if (room) {
+      withSpecifiedRoom(config.ref, function (room) {
         room.actions.participantRequest(user);
-      } else {
-        socket.emit('error', 'Room not found "' + config.ref + '"');
-      }
+      });
     });
     socket.on('open-room', function (config) {
       rooms.create(user, config.name);
@@ -75,16 +45,14 @@ exports.init = function (server) {
       user.disconnect();
     });
     socket.on('request-voting-round', function (config) {
-      var room = rooms.get(config.roomRef);
-      if (!room) {
-        user.sendError('Room not found "' + config.roomRef + '"');
-        return;
-      }
-      room.actions.newVotingRound(config.name, user);
+      withSpecifiedRoom(config.roomRef, function (room) {
+        room.actions.newVotingRound(config.name, user);
+      });
     });
     socket.on('vote', function (config) {
-      var room = rooms.get(config.roomRef);
-      room.actions.voteReceived(user, config.value, config.taskRef);
+      withSpecifiedRoom(config.roomRef, function (room) {
+        room.actions.voteReceived(user, config.value, config.taskRef);
+      });
     });
   });
 };
