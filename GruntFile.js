7e5d09591f6266eb4a210336ab419cc8916fd986
Mat Carey <mat@matcarey.co.uk>
2014-06-14 07:59:02 +0100

    Setup boilerplating.

--- GruntFile.js
+++ GruntFile.js
@@ -3,74 +3,92 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json')
   });
   grunt.initConfig({
-    concurrent: {
-      dev: {
-        tasks: ['watch:jstest', 'watch:css', 'nodemon:local'],
-        options: {
-          logConcurrentOutput: true
-        }
-      }
-    },
-    sass: {
-      dist: {
-        files: {
-          'public/src/style.css': 'src/style.scss'
-        }
-      }
-    },
-    nodemon: {
-      local: {
-        script: 'server/index.js',
-        options: {
-          env: {
-            PORT: '7901'
-          },
-          ext: 'js',
-          watch: ['server']
+      concurrent: {
+        dev: {
+          tasks: ['watch:jstest', 'watch:css', 'nodemon:local'],
+          options: {
+            logConcurrentOutput: true
+          }
         }
       },
-      dev: {
-        script: 'server/index.js',
-        options: {
-          env: {
-            PORT: '8001'
-          },
-          watch: ['none']
+      sass: {
+        dist: {
+          files: {
+            'public/src/style.css': 'src/style.scss'
+          }
         }
       },
-      live: {
-        script: 'server/index.js',
-        options: {
-          env: {
-            PORT: '8101'
-          },
-          watch: ['none']
+      wiredep: {
+        target: {
+          src: 'server/js-dependencies.html'
         }
-      }
-    },
-    jasmine_node: {
-      all: ['test/']
-    },
-    watch: {
-      css: {
-        files: ['src/style.scss'],
-        tasks: ['sass']
       },
-      jstest: {
-        files: ['test/**/*.spec.js', 'public/**/*.js', 'server/*.js'],
-        tasks: ['jasmine_node']
+      "install-dependencies": {
+        dev: {
+          isDevelopment: true
+        },
+        prod: {
+          isDevelopment: true
+        }
+      },
+      nodemon: {
+        local: {
+          script: 'server/index.js',
+          options: {
+            env: {
+              PORT: '7901'
+            },
+            ext: 'js',
+            watch: ['server']
+          }
+        },
+        dev: {
+          script: 'server/index.js',
+          options: {
+            env: {
+              PORT: '8001'
+            },
+            watch: ['none']
+          }
+        },
+        live: {
+          script: 'server/index.js',
+          options: {
+            env: {
+              PORT: '8101'
+            },
+            watch: ['none']
+          }
+        }
+      },
+      jasmine_node: {
+        all: ['test/']
+      },
+      watch: {
+        css: {
+          files: ['src/style.scss'],
+          tasks: ['sass']
+        },
+        jstest: {
+          files: ['test/**/*.spec.js', 'public/**/*.js', 'server/*.js'],
+          tasks: ['jasmine_node']
+        }
       }
     }
-  });
+  )
+  ;
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-wiredep');
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-install-dependencies');
+  grunt.registerTask('precompile', ['sass', 'wiredep']);
   grunt.registerTask('test', ['jasmine_node']);
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['install-dependencies:prod', 'precompile']);
   grunt.registerTask('host-dev', ['nodemon:dev']);
   grunt.registerTask('host-live', ['nodemon:live']);
-  grunt.registerTask('dev', ['jasmine_node', 'sass', 'concurrent']);
+  grunt.registerTask('dev', ['install-dependencies:dev', 'jasmine_node', 'precompile', 'concurrent']);
 }
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/../public'));
+app.use('/bower_components', express.static(__dirname + '/../bower_components'));
 require('./urls').init(app, config);
 require('./serverApi').init(app.listen(config.port));
 console.log('started server on port ', config.port);
